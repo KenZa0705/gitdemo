43e429ade4e1afd21a226091312656f54666370d
Kent Edmark Cuenza <kentedmark@gmail.com>
2023-10-19 14:21:13 +0800

    Modified sql_reference.sql

--- sql_reference.sql
+++ sql_reference.sql
@@ -2,4 +2,11 @@
 
 --DATABASE CREATE DATABASE DB_name;
 
---TABLE CREATE TABLE table_name();
+/*TABLE CREATE TABLE table_name(
+    column_name variable_type constraints
+    column_name variable_type NOT NULL
+
+    -- ex.
+    -- student_id INT PRIMARY KEY
+    
+);*/
